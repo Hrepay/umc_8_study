cdedcb1b2f932d7d68193b5b5d3896ff74c9baaf
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-03-18 14:17:37 +0900

    delete

--- umc_8/Sources/ViewModels/LoginViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LoginViewModel.swift
-//  umc_8
-//
-//  Created by 황상환 on 3/18/25.
-//
-
-import Foundation
--- umc_8/Tests/Umc8Tests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class Umc8Tests: XCTestCase {
-    func test_twoPlusTwo_isFour() {
-        XCTAssertEqual(2+2, 4)
-    }
-}
