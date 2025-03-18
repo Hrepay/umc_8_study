0feb9835dc4c5c9f8898bf32a826285a6b7f3611
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-03-18 15:06:00 +0900

    [egg/week1] week1 파일 추가

---
+++ umc_8/Sources/Models/LoginModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginModel.swift
+//  umc_8
+//
+//  Created by 황상환 on 3/18/25.
+//
+
+import Foundation
+
+struct LoginModel {
+    var id: String
+    var pwd: String
+}
---
+++ umc_8/Sources/Umc8App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Umc8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
